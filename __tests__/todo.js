32015dd0227c6e006de24bd1f92d313cf27178c6
ShaliniAnthati <105448841+ShaliniAnthati@users.noreply.github.com>
2022-09-20 19:28:18 +0530

    Add files via upload

---
+++ __tests__/todo.js
@@ -0,0 +1,35 @@
+const todo = require("../todo");
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todo();
+
+describe("TODO test suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Wake Up",
+      dueDate: "2022-09-17",
+      completed: true,
+    });
+  });
+  test("Add task", () => {
+    let lengthBefore = all.length;
+    add({
+      title: "Eat",
+      dueDate: "2022-09-17",
+      completed: false,
+    });
+    expect(all.length).toBe(lengthBefore + 1);
+  });
+  test("Mark task as complete", () => {
+    all[0].completed = false;
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("Over due tasks", () => {
+    expect(overdue(all)).toBeDefined();
+  });
+  test("Due today tasks", () => {
+    expect(dueToday(all)).toBeDefined();
+  });
+  test("Due later tasks", () => {
+    expect(dueLater(all)).toBeDefined();
+  });
+});
